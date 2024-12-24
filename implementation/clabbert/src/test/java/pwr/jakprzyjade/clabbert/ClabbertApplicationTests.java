c2d21004ea356cf69e82afbe3854b26ddc74fbd4
Jakub Zehner <93779586+jakubzehner@users.noreply.github.com>
2024-12-24 22:02:27 +0100

    PR: Clabbert - Initial Project Setup with Basic Endpoints and Configuration (#44)
    * init clabbert
    
    * change postgres healtcheck
    
    * Update CODEOWNERS
    
    Co-authored-by: Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
    
    ---------
    
    Co-authored-by: Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>

---
+++ implementation/clabbert/src/test/java/pwr/jakprzyjade/clabbert/ClabbertApplicationTests.java
@@ -0,0 +1,16 @@
+package pwr.jakprzyjade.clabbert;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ClabbertApplicationTests {
+
+    @Test
+    void testTest() {
+        Assertions.assertThat(2 + 2).isEqualTo(4);
+    }
+
+}
