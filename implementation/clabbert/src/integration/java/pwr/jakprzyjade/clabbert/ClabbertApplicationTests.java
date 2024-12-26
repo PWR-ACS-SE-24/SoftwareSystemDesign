7d0593158876fab134a9e510d4d509bda5dd779c
jakubzehner <kubusja.pl@gmail.com>
2024-12-26 14:58:39 +0100

    add integration tests

---
+++ implementation/clabbert/src/integration/java/pwr/jakprzyjade/clabbert/ClabbertApplicationTests.java
@@ -0,0 +1,12 @@
+/* @jakubzehner (C) 2024 */
+package pwr.jakprzyjade.clabbert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ClabbertApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
