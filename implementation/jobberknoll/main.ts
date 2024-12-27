821f1aac9817fb4e74d4bd2a56533b43c1b1dce5
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-12-27 18:30:43 +0100

    Set up Docker

--- implementation/jobberknoll/main.ts
+++ implementation/jobberknoll/main.ts
@@ -3,12 +3,14 @@ import { setupProd } from "./setup.ts";
 
 if (import.meta.main) {
   const { app, logger } = setupProd();
-  // TODO @tchojnacki: Set up Dockerfile and compose.yml
   // TODO @tchojnacki: Set up CI/CD
   Deno.serve(
     {
       port: envPort(),
-      onListen: (addr) => logger.info(null, "listen", { addr }),
+      onListen: (addr) =>
+        logger.info(null, "listen", {
+          addr: `http://${addr.hostname}:${addr.port}`,
+        }),
     },
     app.fetch,
   );
