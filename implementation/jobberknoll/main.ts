4f4eb9a1ab2e7c79225adb787c2f386c08ac49f5
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-12-27 17:39:00 +0100

    Support the PORT enviroment variable

--- implementation/jobberknoll/main.ts
+++ implementation/jobberknoll/main.ts
@@ -1,12 +1,15 @@
+import { PORT } from "@jobberknoll/app";
 import { setupProd } from "./setup.ts";
 
 if (import.meta.main) {
   const { app, logger } = setupProd();
-  // TODO @tchojnacki: Support passing PORT from env variables
   // TODO @tchojnacki: Set up Dockerfile and compose.yml
   // TODO @tchojnacki: Set up CI/CD
   Deno.serve(
-    { onListen: (addr) => logger.info(null, "listen", { addr }) },
+    {
+      port: PORT,
+      onListen: (addr) => logger.info(null, "listen", { addr }),
+    },
     app.fetch,
   );
 }
