34ec1adb87e03f27b136d181ecbcca38018333bd
Piotrek Kot <126495976+piterek130@users.noreply.github.com>
2025-01-10 22:15:45 +0100

    PR: Inferius - Initial Project Setup, (#57)
    * project structure
    
    * fix application.yml
    
    * make environment variables consistent

---
+++ implementation/inferius/src/test/java/com/example/inferius/InferiusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inferius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InferiusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
