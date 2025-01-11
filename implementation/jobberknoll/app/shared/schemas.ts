cf0e88034991e3d0336c59dc95cc27de54573a23
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2025-01-11 22:05:46 +0100

    Fix public types for schemas

--- implementation/jobberknoll/app/shared/schemas.ts
+++ implementation/jobberknoll/app/shared/schemas.ts
@@ -1,11 +1,11 @@
 import { z } from "@hono/zod-openapi";
 
-export const FullNameSchema = z.string().min(1).max(255);
+export const FullNameSchema: z.ZodString = z.string().min(1).max(255);
 
-export const EmailSchema = z.string().email().max(255);
+export const EmailSchema: z.ZodString = z.string().email().max(255);
 
 // TODO: what are our password requirements?
-export const PasswordSchema = z.string().min(8).max(64);
+export const PasswordSchema: z.ZodString = z.string().min(8).max(64);
 
 // TODO: what are our phone number constraints?
-export const PhoneNumberSchema = z.string().regex(/^[0-9+ ]{1,16}$/).nullable();
+export const PhoneNumberSchema: z.ZodNullable<z.ZodString> = z.string().regex(/^[0-9+ ]{1,16}$/).nullable();
