f5cec2c3f8b61a3d78269c4b1414e9cce97f0eba
Piotr Kot <piterek130@gmail.com>
2025-01-13 23:57:45 +0100

    fix uuid generating and yml file

--- implementation/phoenix/src/main/java/com/example/phoenix/AddRequestIdFilter.java
+++ implementation/phoenix/src/main/java/com/example/phoenix/AddRequestIdFilter.java
@@ -10,14 +10,17 @@ import java.util.UUID;
 @Component
 public class AddRequestIdFilter extends AbstractGatewayFilterFactory<AddRequestIdFilter.Config> {
 
+    private final UUIDv7Generator uuidv7Generator;
+
     public AddRequestIdFilter() {
         super(Config.class);
+        this.uuidv7Generator = new UUIDv7Generator();
     }
 
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            String requestId = UUID.randomUUID().toString();
+            String requestId = uuidv7Generator.generate();
             ServerWebExchange modifiedExchange = exchange.mutate()
                     .request(r -> r.headers(headers -> headers.add("jp-request-id", requestId)))
                     .build();
