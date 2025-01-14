0c9e816efdb9496ca01b3a238b5a2c96364c74a0
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2025-01-14 22:15:51 +0100

    Fix linter errors

--- implementation/jobberknoll/app/security/passwords.ts
+++ implementation/jobberknoll/app/security/passwords.ts
@@ -8,16 +8,16 @@ const ARGON2ID_OPTIONS = {
   parallelism: 1,
 } satisfies argon2.Options;
 
-export async function hashPassword(userPassword: string) {
+export async function hashPassword(userPassword: string): Promise<string> {
   return await argon2.hash(userPassword, ARGON2ID_OPTIONS);
 }
 
-export async function verifyPassword(userPassword: string, phcHash: string) {
+export async function verifyPassword(userPassword: string, phcHash: string): Promise<boolean> {
   return await argon2.verify(phcHash, userPassword);
 }
 
 // SAFETY: this function is only used in tests
-export function isPasswordHashed(maybePhcHash: string) {
+export function isPasswordHashed(maybePhcHash: string): boolean {
   // NOTE: adapted from https://github.com/P-H-C/phc-string-format/blob/master/phc-sf-spec.md
   return /^\$[a-z0-9-]+(\$v=[0-9]+)?(\$[a-z0-9-]+=[a-zA-Z0-9/+.-]+(,[a-z0-9-]+=[a-zA-Z0-9/+.-]+)*)?(\$[a-zA-Z0-9/+.-]+(\$[A-Za-z0-9+/=]+)?)?$/
     .test(maybePhcHash);
