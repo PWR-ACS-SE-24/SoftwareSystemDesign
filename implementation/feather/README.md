56c15e0b316ac91017158ea07be65a3c980437f0
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2025-01-23 20:45:58 +0100

    Adjust libargon dylib opening

--- implementation/feather/README.md
+++ implementation/feather/README.md
@@ -24,4 +24,3 @@ The service requires the following capabilities to be enabled:
 
 - `--allow-net` - required to host the web server
 - `--allow-env` - required to read config from environment variables
-- `--allow-ffi` - required for Argon2id
--- implementation/jobberknoll/app/security/passwords.ts
+++ implementation/jobberknoll/app/security/passwords.ts
@@ -1,4 +1,4 @@
-import { argon2idHashEncoded, argon2idVerify } from "./libargon2.ts";
+import { Libargon2 } from "./libargon2.ts";
 
 // SAFETY: according to OWASP recommendations (https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#argon2id)
 const ARGON2ID_T_COST = 5;
@@ -10,9 +10,10 @@ const ARGON2ID_SALT_LEN = 16;
 const randomBytes = (length: number) => crypto.getRandomValues(new Uint8Array(length));
 
 export function hashPassword(userPassword: string): Promise<string> {
+  using libargon2 = new Libargon2();
   const saltBuffer = randomBytes(ARGON2ID_SALT_LEN);
   return Promise.resolve(
-    argon2idHashEncoded(
+    libargon2.argon2idHashEncoded(
       ARGON2ID_T_COST,
       ARGON2ID_M_COST,
       ARGON2ID_PARALLELISM,
@@ -24,7 +25,8 @@ export function hashPassword(userPassword: string): Promise<string> {
 }
 
 export function verifyPassword(userPassword: string, phcHash: string): Promise<boolean> {
-  return Promise.resolve(argon2idVerify(phcHash, userPassword));
+  using libargon2 = new Libargon2();
+  return Promise.resolve(libargon2.argon2idVerify(phcHash, userPassword));
 }
 
 // SAFETY: this function is only used in tests
